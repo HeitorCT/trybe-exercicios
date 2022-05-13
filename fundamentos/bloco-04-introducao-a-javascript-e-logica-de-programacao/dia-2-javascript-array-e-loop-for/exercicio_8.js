420d18a0998a437e0a0892bf14621c651365fd97
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-05-13 16:48:24 -0300

    Exercicios de 1 a 9 finalizados | README.md adicionado.

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_8.js
@@ -0,0 +1,6 @@
+let meuArray = [];
+
+for (let i = 1; i < 26; i += 1)
+    meuArray.push(i);
+        
+console.log(meuArray);
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_9.js
@@ -0,0 +1,6 @@
+let meuArray = [];
+
+for (let i = 1; i < 26; i += 1){
+    meuArray.push(i);
+    console.log(i + "/2 = " +  meuArray[i-1]/2);
+}
