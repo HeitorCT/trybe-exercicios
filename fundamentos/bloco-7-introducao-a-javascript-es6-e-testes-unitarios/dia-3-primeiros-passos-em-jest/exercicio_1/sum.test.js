ed07bfa505feee98d29e68c0faa66436d2058eb8
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-07-07 16:32:43 -0300

    iniciando o dia 9.2 - atenção aos arquivos

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio_1/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicio_1/sum.test.js
@@ -1,23 +1,41 @@
-const sum = require("./sum.js");
+// const sum = require("./sum.js");
 
-describe("tests the function that sum two values", () => {
-  describe("simple sum", () => {
-    test("sum 4 and 5 expect 9", () => {
-      expect(sum(4, 5)).toEqual(9);
-    });
+// describe("tests the function that sum two values", () => {
+//   describe("simple sum", () => {
+//     test("sum 4 and 5 expect 9", () => {
+//       expect(sum(4, 5)).toEqual(9);
+//     });
 
-    test("sum 0 and 0 expect 0", () => {
-      expect(sum(0, 0)).toEqual(0);
-    });
-  });
+//     test("sum 0 and 0 expect 0", () => {
+//       expect(sum(0, 0)).toEqual(0);
+//     });
+//   });
   
-  describe("test incorrect parameter", () => {
-    test('sum 4 and "5" expect Error', () => {
-      expect(() => { sum(4, "5"); }).toThrow();
-    });
+//   describe("test incorrect parameter", () => {
+//     test('sum 4 and "5" expect Error', () => {
+//       expect(() => { sum(4, "5"); }).toThrow();
+//     });
 
-    test('sum 4 and "5" expect Error: parameters must be numbers', () => {
-      expect(() => { sum(4, "5"); }).toThrowError(new Error("parameters must be numbers"));
-    });
-  });
-});
+//     test('sum 4 and "5" expect Error: parameters must be numbers', () => {
+//       expect(() => { sum(4, "5"); }).toThrowError(new Error("parameters must be numbers"));
+//     });
+//   });
+// });
+
+// const asyncSum = (a, b, callback) => {
+//   setTimeout(() => {
+//     const result = a + b;
+//     callback(result);
+//   }, 500);
+// };
+
+// test('Testando asyncSum, soma 5 mais 10', (done) => {
+//   asyncSum(5, 10, (result) => {
+//     try {
+//       expect(result).toBe(16);
+//       done();
+//     } catch (error) {
+//       done(error);
+//     }
+//   });
+// });
